e2d789894748305643140e4d37f73f0cffbbe451
kurtisftaylor <kurtisftaylor@gmail.com>
2016-05-07 15:42:19 -0400

    Made small change.

--- HelloWorld/src/src/main/Main.java
+++ HelloWorld/src/src/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	public static void main(String[] args){
 		System.out.println("Hello World!");
-		System.out.println("This has been a test commit and merge.");
+		System.out.println("This has been a test commit and merge - see if it works.");
 	}
 
 }
